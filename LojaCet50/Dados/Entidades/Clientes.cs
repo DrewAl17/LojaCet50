3fc7e05ede21857b273852930d5698c2a2eb99d8
Andre Lajas <Andre.Franco.Lajas@formandos.cinel.pt>
2021-01-07 18:26:06 +0000

    Criar base dados clientes

---
+++ LojaCet50/Dados/Entidades/Clientes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaCet50.Dados.Entidades
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Apelido { get; set; }
+
+        public int Telefone { get; set; }
+
+        public string Morada { get; set; }
+
+        [Display(Name = "Data Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
--- LojaCet50/Migrations/20210107182309_InitBased.Designer.cs
+++ LojaCet50/Migrations/20210107182309_InitBased.Designer.cs
@@ -4,14 +4,16 @@ using LojaCet50.Dados;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LojaCet50.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210107182309_InitBased")]
+    partial class InitBased
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
